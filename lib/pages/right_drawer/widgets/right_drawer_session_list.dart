7d27c0a5c374d2dba405cd32270bec8049c03b89
learning233 <shaguamianbao@gmail.com>
2026-09-10 19:33:51 +0800

    移除了 session name 左侧的图标

--- lib/pages/right_drawer/widgets/right_drawer_session_list.dart
+++ lib/pages/right_drawer/widgets/right_drawer_session_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../controllers/session_controller.dart';
-import '../../../init.dart';
-import '../../../services/e2b_workspace_service.dart';
 import '../../../utils/translations.dart';
 
 /// 右抽屉当前打开的会话列表组件。
@@ -35,15 +33,6 @@ class RightDrawerSessionList extends StatelessWidget {
           return ListTile(
             dense: true,
             selected: isActive,
-            leading: Icon(
-              E2bWorkspaceService.isCloudUrl(Global.serverUrl)
-                  ? Icons.cloud_outlined
-                  : Icons.dns_outlined,
-              size: 18,
-              color: isActive
-                  ? theme.colorScheme.primary
-                  : theme.colorScheme.onSurfaceVariant,
-            ),
             title: Text(name, style: const TextStyle(fontSize: 13)),
             trailing: IconButton(
               icon: const Icon(Icons.close, size: 16),
